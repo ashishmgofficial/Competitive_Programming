171172c7d71c2d456dd13778f6d41ce8c492e548
ashishmgofficial <ashishmg.official@gmail.com>
2019-06-02 23:34:50 +0530

    Bug Fix

--- Coolness_Number.py
+++ Coolness_Number.py
@@ -1,14 +1,13 @@
 
 def coolNum(dec,occurence):
     binaryNum = bin(dec)
-    print(binaryNum)
     result = ""
     cnt=0
     for i,k in enumerate(binaryNum):
         if (i>1):
             result+=str(k)
         if result.__contains__("101"):
-                result=""
+                result=str(1)
                 cnt +=1
 
     print(cnt)
@@ -16,4 +15,4 @@ def coolNum(dec,occurence):
         print("Cool Number")
 
 if __name__ == "__main__":
-    coolNum(45,2)
+    coolNum(22,2)
