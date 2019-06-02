f402f174aca8d5571b0b31bcdd0fb96965c50a4c
ashishmgofficial <ashishmg.official@gmail.com>
2019-06-02 21:49:35 +0530

    Initial Commit : HackerRank Coolness of a number Solution ( Python )

---
+++ learn.py
@@ -0,0 +1,23 @@
+
+def coolNum(dec,occurence):
+    binaryNum = bin(dec)
+    print(binaryNum)
+    result = ""
+    cnt=0
+    for i,k in enumerate(binaryNum):
+        if (i>1):
+            result+=str(k)
+        if result.__contains__("101"):
+                result=""
+                cnt +=1
+
+    print(cnt)
+    if(cnt >= occurence):
+        print("Cool Number")            
+
+
+
+
+
+if __name__ == "__main__":
+    coolNum(45,2)
