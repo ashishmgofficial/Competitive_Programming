f193d2b8c2f8331dbe7f6cc85ec9c2a9e168c63c
Ashish M G <ashishmg.official@gmail.com>
2019-08-17 14:17:30 +0530

    Initial Commit : Base project Structure

---
+++ New_Year_Chaos.py
@@ -0,0 +1,15 @@
+
+def minimumBribes(q):
+    pass
+
+
+
+if __name__= '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
